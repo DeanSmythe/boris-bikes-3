1855ec2d4a0c9b37b4b588433af602546939cd2e
Dean Smythe <dean@teamtwisted.co.uk>
2021-10-13 15:19:45 +0100

    Part 11, not complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,25 @@
 class DockingStation
+  
+  attr_reader :docked_bikes
+
+# def docked_bikes
+#   @docked_bikes
+# end
+
+  def intialize
+  @docked_bikes = []
+  end
+
   def release_bike
     return Bike.new
+  end
+  
+  def dock(bike)
+    puts bike
+    puts @docked_bikes
+    @docked_bikes.push(bike)
+    puts @docked_bikes
+    # return true
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,16 @@ describe DockingStation do
   it 'release_bike should return a bike object' do
     expect(subject.release_bike).to be_instance_of Bike
   end
+  it 'docking station should receive/dock a bike' do
+    my_bike = Bike.new
+    expect(subject.dock(my_bike)).to eq true
+  end
+it 'test to see if a bike has been docked' do
+  my_bike =Bike.new
+  my_docking_station = DockingStation.new
+  my_docking_station.dock(my_bike)
+  # subject.dock(my_bike)
+  expect(my_docking_station.docked_bikes[0]).to eq my_bike
+
+end
 end
